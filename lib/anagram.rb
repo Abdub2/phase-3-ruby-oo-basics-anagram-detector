3e4e1c92dc00056a9729bf986f15bff99e6fb838
Abdub2 <abdubdoyo2005@gmail.com>
2023-02-11 16:39:08 +0300

    Completed assingment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(different_word)
+        different_word.select do |different_word|
+            different_word.split(//).sort == word.split(//).sort
+        end
+    end
+end
